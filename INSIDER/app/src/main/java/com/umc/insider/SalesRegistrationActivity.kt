2651c22a7f169d21a20321c4470da3cbf218f039
limyongsu <lys91892@gmail.com>
2023-07-18 15:48:58 +0900

    Inwoo 판매등록화면 시작

---
+++ INSIDER/app/src/main/java/com/umc/insider/SalesRegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.insider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SalesRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sales_registration)
+    }
+}
