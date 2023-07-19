b88e6b7892c94a313653b6c0c5abf840d462a686
limyongsu <lys91892@gmail.com>
2023-07-19 15:54:17 +0900

    Inwoo 판매등록화면(viewModel 제작)

--- INSIDER/app/src/main/java/com/umc/insider/SalesRegistrationActivity.kt
+++ INSIDER/app/src/main/java/com/umc/insider/SalesRegistrationActivity.kt
@@ -2,10 +2,26 @@ package com.umc.insider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.umc.insider.databinding.ActivitySalesRegistrationBinding
 
 class SalesRegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySalesRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sales_registration)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sales_registration)
+
+
+
+
     }
+
+    companion object{
+        const val REVIEW_MIN_LENGTH = 10
+        // 갤러리 권한 요청
+
+    }
+
 }
