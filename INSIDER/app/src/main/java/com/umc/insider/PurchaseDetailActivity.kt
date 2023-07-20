8af48c1d0c6f9befaa81bdf9f0dee0abaa287256
limyongsu <lys91892@gmail.com>
2023-07-20 13:41:07 +0900

    Inwoo 구매하기 상품 상세 정보 화면(시작)

---
+++ INSIDER/app/src/main/java/com/umc/insider/PurchaseDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.umc.insider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PurchaseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_detail)
+    }
+}
