b59794bbb3a2729e9cd36007cec5faeae0089107
limyongsu <lys91892@gmail.com>
2023-07-20 23:56:57 +0900

    Inwoo 구매하기 상품 상세 정보 화면(ui 찐 완성), 구매하기 화면(시작)

--- INSIDER/app/src/main/java/com/umc/insider/PurchaseDetailActivity.kt
+++ INSIDER/app/src/main/java/com/umc/insider/PurchaseDetailActivity.kt
@@ -2,10 +2,18 @@ package com.umc.insider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.umc.insider.databinding.ActivityPurchaseDetailBinding
 
 class PurchaseDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityPurchaseDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_purchase_detail)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_purchase_detail)
+
+        // 구매하기 버튼 클릭시 mainActivity로 넘어가고 Fragment는 구매하기 PurchaseFragment
+
     }
 }
