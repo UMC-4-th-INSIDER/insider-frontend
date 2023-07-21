f62edbde33724d5decad7723227977375ec0f990
limyongsu <lys91892@gmail.com>
2023-07-21 23:12:32 +0900

    Inwoo 구매하기 화면 및 지도 수정

--- INSIDER/app/src/main/java/com/umc/insider/fragments/PurchaseFragment.kt
+++ INSIDER/app/src/main/java/com/umc/insider/fragments/PurchaseFragment.kt
@@ -1,16 +1,19 @@
 package com.umc.insider.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.MarkerIcons
 import com.umc.insider.R
 import com.umc.insider.databinding.FragmentPurchaseBinding
 
@@ -24,6 +27,8 @@ class PurchaseFragment : Fragment(), OnMapReadyCallback {
     companion object{
         lateinit var naverMap : NaverMap
     }
+
+    private val marker = com.naver.maps.map.overlay.Marker()
 
     private var _binding : FragmentPurchaseBinding? = null
     private val binding get() = _binding!!
@@ -49,7 +54,6 @@ class PurchaseFragment : Fragment(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
 
-
         return binding.root
     }
 
@@ -62,9 +66,17 @@ class PurchaseFragment : Fragment(), OnMapReadyCallback {
         PurchaseFragment.naverMap = naverMap
 
         var camPos = CameraPosition(
-            com.naver.maps.geometry.LatLng(34.38, 128.55),
-            9.0)
+            com.naver.maps.geometry.LatLng(37.485540, 126.802745),
+            15.0)
         naverMap.cameraPosition = camPos
+
+        marker.position = com.naver.maps.geometry.LatLng(37.485540, 126.802745)
+        marker.map = Companion.naverMap
+        marker.icon = MarkerIcons.BLACK
+        marker.iconTintColor = Color.RED
+        marker.width = 70
+        marker.height = 90
+
     }
 
 
