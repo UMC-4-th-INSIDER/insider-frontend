5c1d5525f0878369d628234bc7528fd60abce34a
kms0233 <cnblue789@naver.com>
2023-07-27 10:11:05 +0900

    채팅삭제 UI

--- INSIDER/app/src/main/java/com/umc/insider/fragments/ChatListFragment.kt
+++ INSIDER/app/src/main/java/com/umc/insider/fragments/ChatListFragment.kt
@@ -1,6 +1,7 @@
 package com.umc.insider.fragments
 
 import ChatListAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,32 +9,37 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.umc.insider.R
+import com.umc.insider.DeleteChatActivity
+import com.umc.insider.databinding.FragmentChatListBinding
 import com.umc.insider.model.ChatItem
+import com.umc.insider.saleregistraion.SalesRegistrationActivity
 
 class ChatListFragment : Fragment() {
 
+    private var _binding: FragmentChatListBinding? = null
+    private val binding get() = _binding!!
     private lateinit var chatListAdapter: ChatListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_chat_list, container, false)
+        _binding = FragmentChatListBinding.inflate(inflater, container, false)
 
         // Sample data for ChatList (Replace with your actual data)
         val chatListData = createSampleChatList()
 
         // Initialize RecyclerView and ChatListAdapter
-        val recyclerView: RecyclerView = rootView.findViewById(R.id.chatList)
+        val recyclerView: RecyclerView = binding.chatList
         chatListAdapter = ChatListAdapter(chatListData)
 
         // Set LayoutManager and Adapter for RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = chatListAdapter
 
-        return rootView
+        initView()
+
+        return binding.root
     }
 
     // Sample data for ChatList (Replace with your actual data)
@@ -44,5 +50,19 @@ class ChatListFragment : Fragment() {
         chatList.add(ChatItem("User3", "오늘은 어떤 시간이 가능하신가요?"))
         // Add more chat items as needed
         return chatList
+    }
+
+    private fun initView() {
+        with(binding){
+            deleteTextView.setOnClickListener {
+                val intent = Intent(requireContext(), DeleteChatActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
