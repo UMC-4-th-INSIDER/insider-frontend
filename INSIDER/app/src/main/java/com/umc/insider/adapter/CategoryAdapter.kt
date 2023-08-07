df3b23e6e1b8a191db882274131f44688eae96c1
comst19 <comst19@naver.com>
2023-08-07 17:10:42 +0900

    카테고리 클릭시 검색 로직

--- INSIDER/app/src/main/java/com/umc/insider/adapter/CategoryAdapter.kt
+++ INSIDER/app/src/main/java/com/umc/insider/adapter/CategoryAdapter.kt
@@ -1,6 +1,8 @@
 package com.umc.insider.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -8,14 +10,49 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.umc.insider.R
+import com.umc.insider.utils.CategoryClickListener
 
 class CategoryAdapter(
     private val imageArray: IntArray,
-    private val clickedImageArray: IntArray
+    private val clickedImageArray: IntArray,
+    private val listener: CategoryClickListener
 ) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView : ImageView = itemView.findViewById(R.id.categoryImg)
+        val imageView: ImageView = itemView.findViewById(R.id.categoryImg)
+        private val gestureListener = GestureListener(itemView.context)
+
+        init {
+            imageView.setOnTouchListener { _, event -> gestureListener.onTouch(imageView, event) }
+        }
+
+        inner class GestureListener(context: Context) : GestureDetector.SimpleOnGestureListener() {
+            private val gestureDetector = GestureDetector(context, this)
+
+            fun onTouch(v: View, event: MotionEvent): Boolean {
+                gestureDetector.onTouchEvent(event)
+                return true
+            }
+
+            override fun onDown(e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                imageView.setImageResource(clickedImageArray[adapterPosition])
+                listener.onImageTouch(adapterPosition)
+                return true
+            }
+
+            override fun onScroll(
+                e1: MotionEvent,
+                e2: MotionEvent,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+                return true
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
@@ -26,20 +63,6 @@ class CategoryAdapter(
     @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
         holder.imageView.setImageResource(imageArray[position])
-
-        holder.imageView.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    holder.imageView.setImageResource(clickedImageArray[position])
-                    true
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    holder.imageView.setImageResource(imageArray[position])
-                    true
-                }
-                else -> false
-            }
-        }
     }
 
     override fun getItemCount() = imageArray.size
