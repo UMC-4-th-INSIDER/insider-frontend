ad7c2788c3b24d6278a6c9c51271031f8c825ddf
comst19 <comst19@naver.com>
2023-08-10 22:25:26 +0900

    로그아웃 에러 해결

--- INSIDER/app/src/main/java/com/umc/insider/fragments/MyPageFragment.kt
+++ INSIDER/app/src/main/java/com/umc/insider/fragments/MyPageFragment.kt
@@ -59,7 +59,6 @@ class MyPageFragment : Fragment() {
                 val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
                 val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
                 googleSignInClient.signOut()
-                LoginManager.getInstance().logOut()
                 UserApiClient.instance.logout { error ->
                     if (error != null) {
                         //Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
