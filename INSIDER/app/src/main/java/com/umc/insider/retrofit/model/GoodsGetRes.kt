d852e4e4f20a2feea52e9ed538b885e74a0b63f9
comst19 <comst19@naver.com>
2023-08-15 14:15:54 +0900

    레트로핏 모델, api 정의

--- INSIDER/app/src/main/java/com/umc/insider/retrofit/model/GoodsGetRes.kt
+++ INSIDER/app/src/main/java/com/umc/insider/retrofit/model/GoodsGetRes.kt
@@ -1,8 +1,16 @@
 package com.umc.insider.retrofit.model
 
 import com.google.gson.annotations.SerializedName
+import com.umc.insider.model.Markets
+import com.umc.insider.model.Users
 
 data class GoodsGetRes (
+
+    @SerializedName("users_id")
+    val users_id : Users,
+
+    @SerializedName("markets_id")
+    val markets_id : Markets,
 
     @SerializedName("title")
     val title : String,
@@ -18,6 +26,9 @@ data class GoodsGetRes (
 
     @SerializedName("shelf_life")
     val shelf_life : String,
+
+    @SerializedName("sale")
+    val sale : Integer,
 
     @SerializedName("img_url")
     val img_url : String
