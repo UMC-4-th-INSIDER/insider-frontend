3e918575c3fcf25b728f832b60ecc36b3d24843a
comst19 <comst19@naver.com>
2023-08-18 17:56:46 +0900

    채팅 리스트도 polling방식 추가 - 나중에 최신 메시지를 올릴 예정

--- INSIDER/app/src/main/java/com/umc/insider/fragments/ChatListFragment.kt
+++ INSIDER/app/src/main/java/com/umc/insider/fragments/ChatListFragment.kt
@@ -25,6 +25,9 @@ import com.umc.insider.retrofit.api.GoodsInterface
 import com.umc.insider.utils.ChatListClickListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -35,6 +38,32 @@ class ChatListFragment : Fragment(), ChatListClickListener {
     private var _binding : FragmentChatListBinding? = null
     private val binding get() = _binding!!
     private lateinit var chatListAdapter : ChatListAdapter
+    private val chattingAPI = RetrofitInstance.getInstance().create(ChattingInterface::class.java)
+
+    private var pollingJob: Job? = null
+
+    private fun startPolling() {
+        pollingJob = lifecycleScope.launch {
+            while (isActive) {  // 코루틴이 active한 동안 계속 실행
+                val user_id = UserManager.getUserIdx(requireContext())!!.toLong()
+
+                try {
+                    val response = withContext(Dispatchers.IO) {
+                        chattingAPI.getChatRoomByUser(user_id)
+                    }
+                    if (response.isSuccessful) {
+                        val chatRoomList = response.body()
+                        withContext(Dispatchers.Main) {
+                            chatListAdapter.submitList(chatRoomList)
+                        }
+                    }
+                } catch (e: Exception) {
+                }
+
+                delay(3000L)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,44 +80,29 @@ class ChatListFragment : Fragment(), ChatListClickListener {
             startActivity(Intent(activity, DeleteChatActivity::class.java))
         }
 
-        // Set LayoutManager and Adapter for RecyclerView
         binding.chatList.layoutManager = LinearLayoutManager(requireContext())
         binding.chatList.adapter = chatListAdapter
 
-        val chattingAPI = RetrofitInstance.getInstance().create(ChattingInterface::class.java)
-
-        lifecycleScope.launch {
-
-            val user_id = UserManager.getUserIdx(requireContext())!!.toLong()
-
-            try {
-                val response = withContext(Dispatchers.IO){
-                    chattingAPI.getChatRoomByUser(user_id)
-                }
-                if (response.isSuccessful){
-                    val chatRoomList = response.body()
-                    withContext(Dispatchers.Main) {
-                        chatListAdapter.submitList(chatRoomList)
-                    }
-                }else{
-                }
-            }catch (e : Exception){
-
-            }
-        }
-
         return binding.root
     }
-
-    // Sample data for ChatList (Replace with your actual data)
-
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    private fun stopPolling() {
+        pollingJob?.cancel()
+    }
+    override fun onPause() {
+        super.onPause()
+        stopPolling()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startPolling()
+    }
 
     override fun ChatListItemClick(chatRoomId : Long) {
         val intent = Intent(requireContext(), ChatRoomActivity::class.java)
