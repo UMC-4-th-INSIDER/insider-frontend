04fd518606067bf1328c269dec1b1b9fda539c90
limyongsu <lys91892@gmail.com>
2023-08-21 23:30:52 +0900

    Inwoo 내 정보 수정(프로필 제외 완)

--- INSIDER/app/src/main/java/com/umc/insider/retrofit/api/UserInterface.kt
+++ INSIDER/app/src/main/java/com/umc/insider/retrofit/api/UserInterface.kt
@@ -4,11 +4,14 @@ import com.umc.insider.retrofit.model.LoginPostReq
 import com.umc.insider.retrofit.model.LoginPostRes
 import com.umc.insider.retrofit.model.SignUpPostReq
 import com.umc.insider.retrofit.model.UserGetByIdRes
+import com.umc.insider.retrofit.model.UserPostRes
+import com.umc.insider.retrofit.model.UserPutReq
 import com.umc.insider.retrofit.response.BaseResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface UserInterface {
@@ -22,5 +25,9 @@ interface UserInterface {
 
     @GET("/user/{id}")
     suspend fun getUserById(@Path("id") id: Long): UserGetByIdRes
+
+    @PUT("/user/modify")
+    suspend fun modifyUser(@Body putUserReq: UserPutReq): BaseResponse<UserPostRes>
+
 
 }
