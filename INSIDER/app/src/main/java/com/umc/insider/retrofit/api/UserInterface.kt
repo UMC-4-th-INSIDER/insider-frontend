3cc042b0f96f1cb373e866fd93d574a01e2ffb5d
limyongsu <lys91892@gmail.com>
2023-08-21 22:06:29 +0900

    Inwoo 마이페이지(서버 연결 완)

--- INSIDER/app/src/main/java/com/umc/insider/retrofit/api/UserInterface.kt
+++ INSIDER/app/src/main/java/com/umc/insider/retrofit/api/UserInterface.kt
@@ -3,10 +3,13 @@ package com.umc.insider.retrofit.api
 import com.umc.insider.retrofit.model.LoginPostReq
 import com.umc.insider.retrofit.model.LoginPostRes
 import com.umc.insider.retrofit.model.SignUpPostReq
+import com.umc.insider.retrofit.model.UserGetByIdRes
 import com.umc.insider.retrofit.response.BaseResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface UserInterface {
 
@@ -16,5 +19,8 @@ interface UserInterface {
 
     @POST("/logIn")
     suspend fun logIn(@Body postLoginReq: LoginPostReq): Response<BaseResponse<LoginPostRes>>
+
+    @GET("/user/{id}")
+    suspend fun getUserById(@Path("id") id: Long): UserGetByIdRes
 
 }
