926a29d3f9197642301b386cb9acb0d9c38c37a1
comst19 <comst19@naver.com>
2023-09-01 22:25:36 +0900

    위시리스트 찜 연결 구현

--- INSIDER/app/src/main/java/com/umc/insider/fragments/SearchResultFragment.kt
+++ INSIDER/app/src/main/java/com/umc/insider/fragments/SearchResultFragment.kt
@@ -19,13 +19,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.umc.insider.OnNoteListener
 import com.umc.insider.R
+import com.umc.insider.adapter.ExchangeAdapter
+import com.umc.insider.adapter.ExchangesLongAdapter
 import com.umc.insider.adapter.GoodsLongAdapter
 import com.umc.insider.adapter.SearchResultAdapter
 import com.umc.insider.auth.UserManager
 import com.umc.insider.databinding.FragmentSearchResultBinding
+import com.umc.insider.exchange.ExchangeDetailActivity
+import com.umc.insider.exchange.ExchangeModifyActivity
 import com.umc.insider.model.SearchItem
 import com.umc.insider.purchase.PurchaseDetailActivity
 import com.umc.insider.retrofit.RetrofitInstance
+import com.umc.insider.retrofit.api.ExchangesInterface
 import com.umc.insider.retrofit.api.GoodsInterface
 import com.umc.insider.revise.SaleReviseDetailActivity
 import com.umc.insider.utils.changeStatusBarColor
@@ -40,17 +45,20 @@ class SearchResultFragment : Fragment(), OnNoteListener {
 
     private lateinit var getResultText: ActivityResultLauncher<Intent>
     private val goodsAdapter = GoodsLongAdapter(this)
+    private val exchangesAdapter =  ExchangeAdapter(this)
+
 
     // 일반구매 교환하기 select에서 사용
     private var isGeneralPurchaseSelected = true
     private var isDecorateCheck = true
 
     private val goodsAPI = RetrofitInstance.getInstance().create(GoodsInterface::class.java)
+    private val exchangesAPI = RetrofitInstance.getInstance().create(ExchangesInterface::class.java)
     private var searchQuery : String? = null
 
     override fun onResume() {
         super.onResume()
-        refreshData()
+        updateButtonUI()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -66,34 +74,10 @@ class SearchResultFragment : Fragment(), OnNoteListener {
         _binding = FragmentSearchResultBinding.inflate(inflater, container, false)
 
         initView()
+        updateButtonUI()
         searchQuery = arguments?.getString("search_query")
         binding.searchText.text = "\"$searchQuery\" 검색 결과"
 
-        lifecycleScope.launch {
-
-            try {
-                val response = withContext(Dispatchers.IO) {
-                    goodsAPI.getGoods(searchQuery)
-                }
-                if(response.isSuccess){
-                    val goodsList = response.result
-                    if (goodsList.isNullOrEmpty()) {
-                        Toast.makeText(context, "찾으시는 상품이 없습니다.", Toast.LENGTH_SHORT).show()
-                    } else {
-                        val sortedGoodsList = goodsList.sortedByDescending { it.goods_id }
-                        goodsAdapter.submitList(sortedGoodsList)
-                    }
-
-                }else{
-
-                    // 에러
-                }
-            }catch ( e : Exception){ //
-                //네트워크나 기타 오류
-
-            }
-
-        }
 
         return binding.root
     }
@@ -120,44 +104,11 @@ class SearchResultFragment : Fragment(), OnNoteListener {
                 isGeneralPurchaseSelected = false
                 updateButtonUI()
             }
-            updateButtonUI()
-            isDecorateCheck = false
+
         }
 
-        getResultText =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == RESULT_OK) {
-                    // Handle any result if needed
-                }
-            }
     }
 
-    private fun refreshData(){
-        lifecycleScope.launch {
-
-            try {
-                val response = withContext(Dispatchers.IO) {
-                    goodsAPI.getGoods(searchQuery)
-                }
-                if(response.isSuccess){
-                    val goodsList = response.result
-                    if (goodsList.isNullOrEmpty()) {
-                        Toast.makeText(context, "찾으시는 상품이 없습니다.", Toast.LENGTH_SHORT).show()
-                    } else {
-                        val sortedGoodsList = goodsList.sortedByDescending { it.goods_id }
-                        goodsAdapter.submitList(sortedGoodsList)
-                    }
-
-                }else{
-
-                    // 에러
-                }
-            }catch ( e : Exception){ //
-                //네트워크나 기타 오류
-
-            }
-        }
-    }
 
 
     override fun onDestroyView() {
@@ -169,30 +120,59 @@ class SearchResultFragment : Fragment(), OnNoteListener {
 
         val userIdx = UserManager.getUserIdx(requireActivity().applicationContext)!!.toLong()
         Log.d("REVISEEE", "userIdx : {$userIdx}")
-        lifecycleScope.launch {
-            try {
-                val response = withContext(Dispatchers.IO){
-                    goodsAPI.getGoodsById(goods_id)
-                }
-                withContext(Dispatchers.Main){
-                    val sellerID = response.users_id.id
 
-                    if(userIdx != sellerID){
-                        //Toast.makeText(requireContext(), goods_id.toString(), Toast.LENGTH_SHORT).show()
-                        val intent = Intent(requireContext(), PurchaseDetailActivity::class.java)
-                        intent.putExtra("goods_id", goods_id.toString())
-                        startActivity(intent)
-                    } else {
-                        val intent = Intent(requireContext(), SaleReviseDetailActivity::class.java)
-                        intent.putExtra("goods_id", goods_id.toString())
-                        startActivity(intent)
+        if(isGeneralPurchaseSelected){
+            lifecycleScope.launch {
+                try {
+                    val response = withContext(Dispatchers.IO){
+                        goodsAPI.getGoodsById(goods_id)
                     }
+                    withContext(Dispatchers.Main){
+                        val sellerID = response.users_id.id
+
+                        if(userIdx != sellerID){
+                            //Toast.makeText(requireContext(), goods_id.toString(), Toast.LENGTH_SHORT).show()
+                            val intent = Intent(requireContext(), PurchaseDetailActivity::class.java)
+                            intent.putExtra("goods_id", goods_id.toString())
+                            startActivity(intent)
+                        } else {
+                            val intent = Intent(requireContext(), SaleReviseDetailActivity::class.java)
+                            intent.putExtra("goods_id", goods_id.toString())
+                            startActivity(intent)
+                        }
+                    }
+                }catch (e : Exception){
+
                 }
-            }catch (e : Exception){
 
             }
+        }else{
+            lifecycleScope.launch {
+                try {
+                    val response = withContext(Dispatchers.IO){
+                        exchangesAPI.getGoodsById(goods_id)
+                    }
+                    withContext(Dispatchers.Main){
+                        val sellerID = response.user.id
 
+                        if(userIdx != sellerID){
+                            //Toast.makeText(requireContext(), goods_id.toString(), Toast.LENGTH_SHORT).show()
+                            val intent = Intent(requireContext(), ExchangeDetailActivity::class.java)
+                            intent.putExtra("goods_id", goods_id.toString())
+                            startActivity(intent)
+                        } else {
+                            val intent = Intent(requireContext(), ExchangeModifyActivity::class.java)
+                            intent.putExtra("goods_id", goods_id.toString())
+                            startActivity(intent)
+                        }
+                    }
+                }catch (e : Exception){
+
+                }
+
+            }
         }
+
 
     }
 
@@ -205,11 +185,65 @@ class SearchResultFragment : Fragment(), OnNoteListener {
                 Exchange.background = ContextCompat.getDrawable(requireContext(), R.drawable.white_right_round)
                 Exchange.setTextColor(ContextCompat.getColor(requireContext(), R.color.main))
 
+                searchRV.adapter = goodsAdapter
+                searchRV.layoutManager = LinearLayoutManager(context)
+
+                lifecycleScope.launch {
+
+                    try {
+                        val response = withContext(Dispatchers.IO) {
+                            goodsAPI.getGoods(searchQuery)
+                        }
+                        if(response.isSuccess){
+                            val goodsList = response.result
+                            if (goodsList.isNullOrEmpty()) {
+                                Toast.makeText(context, "찾으시는 상품이 없습니다.", Toast.LENGTH_SHORT).show()
+                            } else {
+                                val sortedGoodsList = goodsList.sortedByDescending { it.goods_id }
+                                goodsAdapter.submitList(sortedGoodsList)
+                            }
+
+                        }else{
+
+                            // 에러
+                        }
+                    }catch ( e : Exception){ //
+                        //네트워크나 기타 오류
+                    }
+                }
+
             } else {
                 generalPurchase.background = ContextCompat.getDrawable(requireContext(), R.drawable.white_left_round)
                 generalPurchase.setTextColor(ContextCompat.getColor(requireContext(), R.color.main))
                 Exchange.background = ContextCompat.getDrawable(requireContext(), R.drawable.green_right_round)
                 Exchange.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+
+                searchRV.adapter = exchangesAdapter
+                searchRV.layoutManager = LinearLayoutManager(context)
+
+                lifecycleScope.launch {
+
+                    try {
+                        val response = withContext(Dispatchers.IO) {
+                            exchangesAPI.getAllExchanges(searchQuery)
+                        }
+                        if(response.isSuccess){
+                            val goodsList = response.result
+                            if (goodsList.isNullOrEmpty()) {
+                                Toast.makeText(context, "찾으시는 상품이 없습니다.", Toast.LENGTH_SHORT).show()
+                            } else {
+                                val sortedGoodsList = goodsList.sortedByDescending { it.id }
+                                exchangesAdapter.submitList(sortedGoodsList)
+                            }
+
+                        }else{
+
+                            // 에러
+                        }
+                    }catch ( e : Exception){ //
+                        //네트워크나 기타 오류
+                    }
+                }
 
             }
         }
